67f2d9f91d12905a49903b172614a4fcf6bb8b5c
henriquet kapema <henriquetkapema@gmail.com>
2024-08-08 00:33:49 +0100

    db2

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -3,22 +3,22 @@ import mongoose from 'mongoose';
 let initialized = false;
 
 export const connect = async () => {
-    mongoose.set('strictQuery', true); 
+  mongoose.set('strictQuery', true);
 
-    if (initialized) {
-        console.log('MongoDB already connected'); 
-        return
-    }
+  if (initialized) {
+    console.log('MongoDB already connected');
+    return;
+  }
 
-    try {
-       await mongoose.connect(process.env.MONGODB_URI, {
-        dbName: "next-auth-app",
-        userNewUrlParser: true, 
-        useUnifiedTopology: true, 
-       }); 
-       console.log("MongoDB connected"); 
-       initialized = true; 
-    } catch(error){
-        console.log('MongoDB connection error:', error); 
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'next-auth-app',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+    initialized = true;
+  } catch (error) {
+    console.log('MongoDB connection error:', error);
+  }
+};
